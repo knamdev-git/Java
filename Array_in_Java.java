c9af80d0b3dfc3421a961cb80b983e561d02908a
Kanha Namdev <147715645+knamdev-git@users.noreply.github.com>
2024-07-04 10:36:26 +0530

    Array_in_Java.java

--- Array_in_Java.java
+++ Array_in_Java.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Array_in_Java {
     public static void main(String[] args) {
        
-        //New (Keyword) : use to to take new space in memeory which is non premitive data 
-        int marks[] = new int[5];
+        // New (Keyword) : Use to to take new space in memeory for non premitive data 
+        int marks[] = new int[5]; // After new " int[5] " : 5 , describes the size of an array we created 
         
-        //
+        // Printing the statement we have to show to the screen 
         System.out.println("Enter five marks ");
         
-        //taking input 
+        // Taking input from the user  
         Scanner scan = new Scanner(System.in) ;
         
-        for (int i = 0; i < marks.length; i++) {
+        for (int i = 0; i < marks.length; i++) { //marks.length calls the length of an array , In java we can directly calculate the length with the help of .length() func
          marks[i] = scan.nextInt();   
         }
 
